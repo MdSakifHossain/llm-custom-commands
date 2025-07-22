5e60987c5e7fb2fd705fa1b604ef7b263b653e1c
MdSakifHossain <sakif015sakif@gmail.com>
2025-07-22 16:00:41 +0600

    refactor: Remove development utility component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import ProjectIdeaSection from "./components/sections/ProjectIdeaSection.jsx";
 import FooterSection from "./components/sections/FooterSection.jsx";
 
 // utility component to check for the vp width and tell me in which viewpoert i am currently in.. eg. sm, md, lg, etc...
-import BreakpointChecker from "./components/utils/BreakpointChecker.jsx";
+// import BreakpointChecker from "./components/utils/BreakpointChecker.jsx";
 
 const App = () => {
   return (
     <>
-      <BreakpointChecker />
+      {/* <BreakpointChecker /> */}
       <HeaderSection />
       <MainSection />
       <ProjectIdeaSection />
